8103d811be4addbfebc2e5f431449237c20c61fa
Joshua Gato <joshuagatogato37@gmail.com>
2020-04-13 09:29:20 +0000

    travis-ci and flake8 added

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,10 @@
-""" Add two numbers together """
+
+
 def add(x, y):
+    """ Add two numbers together """
     return x + y
+
+
+def subtract(x, y):
+    """ Subtract x from y and return value """
+    return y - x
