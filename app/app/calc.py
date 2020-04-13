7515a490b01cda634bef522279b798c735e857a9
Joshua Gato <joshuagatogato37@gmail.com>
2020-04-13 08:56:34 +0000

    test written

---
+++ app/app/calc.py
@@ -0,0 +1,3 @@
+""" Add two numbers together """
+def add(x, y):
+    return x + y
---
+++ app/app/test.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from app.calc import add
+
+class CalcTests(TestCase):
+
+    """" Test that two numbers are added together """
+    def test_add_numbers(self):
+        self.assertEqual(add(3, 8), 11)
